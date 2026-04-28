0cd4c9172d21ef9b187f22ae2122984da288f4a4
Aditya Jha <a97947681@gmail.com>
2026-04-28 14:58:18 +0530

    Update exp4.py

--- exp4.py
+++ exp4.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('Star.png', 0)
+img = cv2.imread('image.jpeg', 0)
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
 rows, cols = img.shape
