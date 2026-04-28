cd9d40a2815bf04444b479917f14a6ca573eca38
Aditya Jha <a97947681@gmail.com>
2026-04-28 14:58:35 +0530

    Update exp5.py

--- exp5.py
+++ exp5.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('Star.png', 0)
+img = cv2.imread('image.jpeg', 0)
 
 # Gaussian Noise
 gauss = img + np.random.normal(0, 20, img.shape)
